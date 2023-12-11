ff4ba867058c84eda69b8f1bf5a60210ee961a64
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-12-11 12:14:30 +0100

    Modification  basket classes

--- src/main/java/com/example/Book_Store/service/BasketService.java
+++ src/main/java/com/example/Book_Store/service/BasketService.java
@@ -1,8 +1,20 @@
 package com.example.Book_Store.service;
 
 import com.example.Book_Store.entities.Basket;
+import com.example.Book_Store.entities.Book;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface BasketService {
-    Basket findByIdBook(Long id);
+    Basket findByIdBook(Integer id);
+
     Basket saveBasket(Basket basket);
+
+
+
+
+    Basket findBasketByUserId(Integer id);
 }
--- src/main/java/com/example/Book_Store/service/implementation/BasketServiceImpl.java
+++ src/main/java/com/example/Book_Store/service/implementation/BasketServiceImpl.java
@@ -1,27 +1,46 @@
 package com.example.Book_Store.service.implementation;
 
 import com.example.Book_Store.entities.Basket;
+import com.example.Book_Store.entities.Book;
 import com.example.Book_Store.repository.BasketRepository;
 import com.example.Book_Store.service.BasketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BasketServiceImpl implements BasketService {
 
-    BasketRepository basketRepository;
+   private final BasketRepository basketRepository;
+
     @Autowired
     public BasketServiceImpl(BasketRepository basketRepository) {
         this.basketRepository = basketRepository;
     }
 
     @Override
-    public Basket findByIdBook(Long id) {
-        return basketRepository.findByIdBook(id);
+    public Basket findByIdBook(Integer id) {
+        return null;
     }
 
     @Override
     public Basket saveBasket(Basket basket) {
         return basketRepository.save(basket);
     }
+
+    @Override
+    public Basket findBasketByUserId(Integer id) {
+        return basketRepository.findBasketByUserId(id);
+    }
+
+
 }
+
+
+
+
+
