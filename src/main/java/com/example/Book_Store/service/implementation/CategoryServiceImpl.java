61874bdcb2d89694c90affa6dcfed04eaab34355
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-12-11 12:16:20 +0100

    Modification and update category classes

--- src/main/java/com/example/Book_Store/service/implementation/CategoryServiceImpl.java
+++ src/main/java/com/example/Book_Store/service/implementation/CategoryServiceImpl.java
@@ -3,37 +3,37 @@ package com.example.Book_Store.service.implementation;
 import com.example.Book_Store.entities.Category;
 import com.example.Book_Store.repository.CategoryRepository;
 import com.example.Book_Store.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    CategoryRepository categoryRepository;
-    @Autowired
+    private final CategoryRepository categoryRepository;
+
     public CategoryServiceImpl(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public Category findCategoryById(Long id) {
+    public Optional<Category> findCategoryById(Integer id) {
         return categoryRepository.findCategoryById(id);
     }
 
     @Override
-    public Category findCategoryByName(String name) {
+    public Optional<Category> findCategoryByName(String name) {
         return categoryRepository.findCategoryByName(name);
     }
 
     @Override
-    public void deleteCategoryById(Long id) {
+    public void deleteCategoryById(Integer id) {
     categoryRepository.deleteCategoryById(id);
     }
 
     @Override
     public Category createCategory(Category category) {
-        return categoryRepository.save(category);
+        return categoryRepository.save(category) ;
     }
 
     @Override
@@ -43,11 +43,17 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void deleteAllCategories() {
-    categoryRepository.deleteAll();
+        categoryRepository.deleteAll();
     }
 
     @Override
     public Category updateCategory(Category category) {
         return categoryRepository.save(category);
     }
+
+    @Override
+    public boolean categoryExistsById(Integer id) {
+        return categoryRepository.existsById(id);
+    }
+
 }
