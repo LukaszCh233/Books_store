53f9b219c81dcb1d7d2b0ec5c041e0d92c9c70b3
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-01-22 11:09:22 +0100

    Update RegisterLoginController class

--- src/main/java/com/example/Book_Store/controller/RegisterLoginController.java
+++ src/main/java/com/example/Book_Store/controller/RegisterLoginController.java
@@ -6,11 +6,13 @@ import com.example.Book_Store.config.JwtTokenServiceImpl;
 import com.example.Book_Store.entities.Admin;
 import com.example.Book_Store.entities.Customer;
 import com.example.Book_Store.entities.CustomerLogin;
-import com.example.Book_Store.service.AdminService;
 import com.example.Book_Store.service.CustomerLoginService;
-import com.example.Book_Store.service.CustomerService;
+import com.example.Book_Store.service.implementation.AdminServiceImpl;
+import com.example.Book_Store.service.implementation.CustomerLoginServiceImpl;
+import com.example.Book_Store.service.implementation.CustomerServiceImpl;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,8 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/access")
@@ -27,13 +28,13 @@ public class RegisterLoginController {
     private final HelpJwt helpJwt;
     private final Logger logger = (Logger) LoggerFactory.getLogger(RegisterLoginController.class);
     CustomerLoginService customerLoginService;
-    CustomerService customerService;
-    AdminService adminService;
+    CustomerServiceImpl customerService;
+    AdminServiceImpl adminService;
     JwtTokenServiceImpl jwtTokenService;
     PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegisterLoginController(HelpJwt helpJwt, CustomerLoginService customerLoginService, CustomerService customerService, AdminService adminService, JwtTokenServiceImpl jwtTokenService, PasswordEncoder passwordEncoder) {
+    public RegisterLoginController(HelpJwt helpJwt, CustomerLoginServiceImpl customerLoginService, CustomerServiceImpl customerService, AdminServiceImpl adminService, JwtTokenServiceImpl jwtTokenService, PasswordEncoder passwordEncoder) {
         this.helpJwt = helpJwt;
         this.customerLoginService = customerLoginService;
         this.customerService = customerService;
@@ -44,84 +45,46 @@ public class RegisterLoginController {
 
     @PostMapping("/customerReg")
     ResponseEntity<?> registerCustomer(@RequestBody Customer customer) {
-        boolean checkEmail = customerLoginService.existsByEmail(customer.getCustomerLogin().getEmail());
-        if (checkEmail) {
-            logger.warn("Email already exists: {}", customer.getCustomerLogin().getEmail());
-            return new ResponseEntity<>("Email exists", HttpStatus.BAD_REQUEST);
-        } else {
-
-            Customer create = customerService.createCustomer(customer);
-            logger.info("User registered successfully: {}", create.getCustomerLogin().getEmail());
-            return new ResponseEntity<>(create, HttpStatus.OK);
-        }
+        Customer createCustomer = customerService.createCustomer(customer);
+        CustomerDTO customerDTO = customerService.mapCustomerToCustomerDTO(createCustomer);
+        logger.info("User registered successfully: {}", createCustomer.getCustomerLogin().getEmail());
+        return new ResponseEntity<>(customerDTO, HttpStatus.OK);
     }
 
     @PostMapping("/adminReg")
     ResponseEntity<?> registerAdmin(@RequestBody Admin admin) {
-        try {
 
-
-            boolean checkEmail = adminService.existsByEmail(admin.getEmail());
-            if (checkEmail) {
-                logger.warn("Email already exists: {}", admin.getEmail());
-                return new ResponseEntity<>("Email exists", HttpStatus.BAD_REQUEST);
-            } else {
-
-                Admin create = adminService.createAdmin(admin);
-                logger.info("User registered successfully: {}", create.getEmail());
-                return new ResponseEntity<>(create, HttpStatus.OK);
-            }
-        } catch (Exception e) {
-            logger.error("Error: {}", e.getMessage(), e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-
-        }
+        Admin createAdmin = adminService.createAdmin(admin);
+        AdminDTO adminDTO = adminService.mapAdminToAdminDTO(createAdmin);
+        logger.info("User registered successfully: {}", createAdmin.getEmail());
+        return new ResponseEntity<>(adminDTO, HttpStatus.OK);
     }
-
 
     @PostMapping("/customerLog")
     ResponseEntity<?> loginCustomer(@RequestBody CustomerLogin customer) {
+        Customer registerCustomer = customerService.findByEmail(customer.getEmail()).orElseThrow(()
+                -> new ResourceNotFoundException("Customer not exists"));
 
-        Optional<Customer> storedUserOptional = customerService.findByEmail(customer.getEmail());
-
-        if (storedUserOptional.isPresent()) {
-
-            Customer storedUser = storedUserOptional.get();
-            if (passwordEncoder.matches(customer.getPassword(), storedUser.getPassword())) {
-                logger.info("User logged in successfully: {}", storedUser.getCustomerLogin().getEmail());
-                String jwtToken = helpJwt.generateToken(storedUser);
-
-                return new ResponseEntity<>(jwtToken, HttpStatus.OK);
-            } else {
-                logger.warn("Incorrect password for user: {}", storedUser.getCustomerLogin().getEmail());
-                return new ResponseEntity<>("Incorrect password", HttpStatus.UNAUTHORIZED);
-            }
-        } else {
-            logger.warn("User not found for email: {}", customer.getEmail());
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        if (!passwordEncoder.matches(customer.getPassword(), registerCustomer.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Incorrect email or password");
         }
+        String jwtToken = helpJwt.generateToken(registerCustomer);
+        logger.info("User logged in successfully: {}", registerCustomer.getCustomerLogin().getEmail());
+        return ResponseEntity.ok(jwtToken);
     }
 
     @PostMapping("/adminLog")
     ResponseEntity<?> loginAdmin(@RequestBody Admin admin) {
 
-        Optional<Admin> storedUserOptional = adminService.findByEmail(admin.getEmail());
+        Admin registeredAdmin = adminService.findByEmail(admin.getEmail()).orElseThrow(()
+                -> new ResourceNotFoundException("Admin not exists"));
 
-        if (storedUserOptional.isPresent()) {
-
-            Admin storedUser = storedUserOptional.get();
-            if (passwordEncoder.matches(admin.getPassword(), storedUser.getPassword())) {
-                logger.info("User logged in successfully: {}", storedUser.getEmail());
-                String jwtToken = helpJwt.generateToken(storedUser);
-
-                return new ResponseEntity<>(jwtToken, HttpStatus.OK);
-            } else {
-                logger.warn("Incorrect password for user: {}", storedUser.getEmail());
-                return new ResponseEntity<>("Incorrect password", HttpStatus.UNAUTHORIZED);
-            }
-        } else {
-            logger.warn("User not found for email: {}", admin.getEmail());
-            return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        if (!passwordEncoder.matches(admin.getPassword(), registeredAdmin.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Incorrect email or password");
         }
+        String jwtToken = helpJwt.generateToken(registeredAdmin);
+        logger.info("User logged in successfully: {}", registeredAdmin.getEmail());
+        return ResponseEntity.ok(jwtToken);
+
     }
 }
