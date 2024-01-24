464a5e03ec21ca4ce4ed7232d2be958cca5d6c96
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-01-24 14:49:13 +0100

    Update order function

--- src/main/java/com/example/Book_Store/service/implementation/OrderServiceImpl.java
+++ src/main/java/com/example/Book_Store/service/implementation/OrderServiceImpl.java
@@ -1,24 +1,38 @@
 package com.example.Book_Store.service.implementation;
 
 import com.example.Book_Store.controller.OrderDTO;
-import com.example.Book_Store.entities.Order;
-import com.example.Book_Store.entities.Status;
+import com.example.Book_Store.entities.*;
+import com.example.Book_Store.repository.CustomerRepository;
 import com.example.Book_Store.repository.OrderRepository;
+import com.example.Book_Store.repository.OrderedBooksRepository;
 import com.example.Book_Store.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
-    OrderRepository orderRepository;
+    private final OrderRepository orderRepository;
+    private final BasketServiceImpl basketService;
+    private final CustomerRepository customerRepository;
+    private final OrderedBooksRepository orderedBooksRepository;
+    private final BookServiceImpl bookService;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository, BasketServiceImpl basketService, CustomerRepository customerRepository,
+                            OrderedBooksRepository orderedBooksRepository, BookServiceImpl bookService) {
         this.orderRepository = orderRepository;
+        this.basketService = basketService;
+        this.customerRepository = customerRepository;
+        this.orderedBooksRepository = orderedBooksRepository;
+        this.bookService = bookService;
 
 
     }
@@ -64,6 +78,49 @@ public class OrderServiceImpl implements OrderService {
             throw new ResourceNotFoundException("Order with this ID has already been sent");
         }
         order.setStatus(Status.SENT);
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Order saveOrder(Order order, Principal principal) {
+        Basket basket = basketService.findBasketByUserId(principal);
+
+        if (basket.getBasketProducts().isEmpty()) {
+            throw new ResourceNotFoundException("Basket is empty");
+        }
+        Customer customer = customerRepository.findById(basket.getUserId())
+                .orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
+
+        order.setCustomer(customer);
+        order.setOrderData(LocalDate.now());
+        order.setOrderedBooks(new ArrayList<>());
+
+        double totalPrice = basket.getBasketProducts().stream().mapToDouble(BasketProducts::getPrice)
+                .sum();
+        order.setPrice(totalPrice);
+        orderRepository.save(order);
+
+
+        List<OrderedBooks> orderedBooksList = basket.getBasketProducts().stream().map(basketProducts -> {
+            OrderedBooks orderedBooks = new OrderedBooks();
+            orderedBooks.setOrder(order);
+            orderedBooks.setIdBook(basketProducts.getIdBook());
+            orderedBooks.setQuantity(basketProducts.getQuantity());
+            return orderedBooks;
+        }).toList();
+
+        orderedBooksRepository.saveAll(orderedBooksList);
+        order.setOrderedBooks(orderedBooksList);
+        order.setStatus(Status.ORDERED);
+
+        for (OrderedBooks orderedBooks : orderedBooksList) {
+            Book book = bookService.findBookById(orderedBooks.getIdBook());
+
+            int newQuantity = book.getQuantity() - orderedBooks.getQuantity();
+            book.setQuantity(newQuantity);
+            bookService.updateBook(book.getId(), book);
+        }
+        basketService.deleteBasketById(principal);
         return orderRepository.save(order);
     }
 }
