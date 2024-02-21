45df215f02ce026d8d946c6f5e96910ca4e662b1
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-02-21 14:50:55 +0100

    Add controller tests

---
+++ src/test/java/com/example/Book_Store/controllerTests/CommonControllerTest.java
@@ -0,0 +1,180 @@
+package com.example.Book_Store.controllerTests;
+
+import com.example.Book_Store.config.HelpJwt;
+import com.example.Book_Store.entities.Book;
+import com.example.Book_Store.entities.Category;
+import com.example.Book_Store.service.implementation.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class CommonControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    CategoryServiceImpl categoryService;
+    @MockBean
+    BookServiceImpl bookService;
+    @MockBean
+    CustomerServiceImpl customerService;
+    @MockBean
+    OrderServiceImpl orderService;
+    @MockBean
+    BasketServiceImpl basketService;
+    @Autowired
+    WebApplicationContext context;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+    @MockBean
+    private HelpJwt helpJwt;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @WithMockUser(username = "customer", roles = {"CUSTOMER"})
+    public @interface WithMockCustomer {
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetCategories_Test() throws Exception {
+        List<Category> categories = Arrays.asList(new Category(1, "testName"), new Category(2, "testName1"));
+
+        when(categoryService.findAllCategories()).thenReturn(categories);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getCategories")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].name").value("testName"))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].name").value("testName1"));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetBooks_Test() throws Exception {
+        List<Book> books = Arrays.asList(new Book(1, null, null, 10, 10, null, null), new Book(2, null, null, 50, 20, null, null));
+
+        when(bookService.findAllBooks()).thenReturn(books);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getBooks")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].price").value(10))
+                .andExpect(jsonPath("$[0].quantity").value(10))
+                .andExpect(jsonPath("$[1].price").value(50))
+                .andExpect(jsonPath("$[1].quantity").value(20));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetCategoryByName_Test() throws Exception {
+        Category category = new Category(1, "testName");
+
+        when(categoryService.findCategoryByName(category.getName())).thenReturn(category);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getCategoryByName/{name}", category.getName())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"testName\"}")
+                        .content("{\"id\":\"1\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetBookByTitle_Test() throws Exception {
+        Book book = new Book(1, "testTitle", null, 10, 10, null, null);
+
+        when(bookService.findByTitle(book.getTitle())).thenReturn(Collections.singletonList(book));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getBook/title/{title}", book.getTitle())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":\"1\"}")
+                        .content("{\"title\":\"1testTitle\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetBookById_Test() throws Exception {
+        Book book = new Book(1, "testTitle", null, 10, 10, null, null);
+
+        when(bookService.findBookById(book.getId())).thenReturn((book));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getBook/title/{title}", book.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":\"1\"}")
+                        .content("{\"title\":\"1testTitle\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetBooksByCategoryName_Test() throws Exception {
+        Category category = new Category(1, "testCategory");
+        Book book = new Book(1, "testTitle", null, 10, 10, null, category);
+
+        when(bookService.findByCategoryName(category.getName())).thenReturn(Collections.singletonList(book));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getBooks/categoryName/{name}", book.getCategory())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":\"1\"}")
+                        .content("{\"title\":\"1testTitle\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldGetBooksByCategoryId_Test() throws Exception {
+        Category category = new Category(1, "testCategory");
+        Book book = new Book(1, "testTitle", null, 10, 10, null, category);
+
+        when(bookService.findByCategoryId(category.getId())).thenReturn(Collections.singletonList(book));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/common/getBooks/categoryId/{id}", book.getCategory().getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"id\":\"1\"}")
+                        .content("{\"title\":\"1testTitle\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/example/Book_Store/controllerTests/CustomerControllerTest.java
@@ -0,0 +1,159 @@
+package com.example.Book_Store.controllerTests;
+
+import com.example.Book_Store.config.HelpJwt;
+import com.example.Book_Store.controller.BasketDTO;
+import com.example.Book_Store.entities.Basket;
+import com.example.Book_Store.entities.BasketProducts;
+import com.example.Book_Store.entities.Order;
+import com.example.Book_Store.service.implementation.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    CategoryServiceImpl categoryService;
+    @MockBean
+    BookServiceImpl bookService;
+    @MockBean
+    CustomerServiceImpl customerService;
+    @MockBean
+    OrderServiceImpl orderService;
+    @MockBean
+    BasketServiceImpl basketService;
+
+    @Autowired
+    WebApplicationContext context;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+    @MockBean
+    private HelpJwt helpJwt;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @WithMockUser(username = "customer", roles = {"CUSTOMER"})
+    public @interface WithMockCustomer {
+
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldAddToBasket_Test() throws Exception {
+        Principal principal = mock(Principal.class);
+
+        Basket basket = new Basket(100.0, 1, 1, null);
+        BasketProducts basketProducts = new BasketProducts(1, basket, 1, "testName", "testAuthor", 10.0, 10);
+
+        doNothing().when(basketService).addBookToBasket(basketProducts.getIdBook(), 10, principal);
+
+        mockMvc.perform(post("/customer/toBasket/{idBook}/{quantity}", basketProducts.getIdBook(), 10)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Book added to basket"));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldOrder_Test() throws Exception {
+        Principal principal = mock(Principal.class);
+        Order order = new Order(1, null, LocalDate.now(), 100.0, null, null);
+
+        when(orderService.saveOrder(order, principal)).thenReturn(order);
+
+        mockMvc.perform(post("/customer/order")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("order is completed"));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldDeleteBasket_test() throws Exception {
+        Principal principal = mock(Principal.class);
+
+        doNothing().when(basketService).deleteBasketById(principal);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/customer/deleteBasket")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Basket deleted successfully"));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldDisplayBasket_Test() throws Exception {
+
+        Principal principal = mock(Principal.class);
+        BasketDTO mockBasketDTO = new BasketDTO();
+        mockBasketDTO.setIdBasket(1);
+        mockBasketDTO.setBasketProducts(new ArrayList<>());
+        mockBasketDTO.setTotalPrice(100.0);
+        mockBasketDTO.setUserId(1);
+
+        when(basketService.findBasketDTOByUserPrincipal(any(Principal.class))).thenReturn(mockBasketDTO);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/getBasket")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.totalPrice").value("100.0"));
+    }
+
+    @WithMockCustomer
+    @Test
+    void shouldUpdateBasket_Test() throws Exception {
+        Principal principal = mock(Principal.class);
+        Basket basket = new Basket();
+        BasketProducts basketProducts = new BasketProducts(1, basket, 1, null, null, 10.0, 10);
+        BasketProducts updateBasketProduct = new BasketProducts(null, basket, 1, null, null, 10.0, 5);
+
+        doNothing().when(basketService).updateBasket(basketProducts.getId(), 5, principal);
+
+        mockMvc.perform(put("/customer/updateBasket/{id}/{quantity}", 1, 5)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"quantity\":\"5\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
+
+
+
